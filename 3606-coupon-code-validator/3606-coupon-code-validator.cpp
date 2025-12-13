fc39f103450b372a1925ddfd458acb3b80599c02
arwa-28 <arwam4307@gmail.com>
2025-12-13 07:47:09 +0200

    Time: 11 ms (30.04%), Space: 69 MB (63.95%) - LeetHub

---
+++ 3606-coupon-code-validator/3606-coupon-code-validator.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        vector <pair <string, string>> v;   
+        
+        for (int i = 0; i < code.size(); i++){
+            if (businessLine[i] != "electronics" && businessLine[i] != "grocery" && businessLine[i] != "pharmacy" && businessLine[i] != "restaurant") continue;
+            bool ok = 1;
+            for (auto &c : code[i]){
+                if (!('a' <= c && c <= 'z') && !('A' <= c && c <= 'Z') && !('0' <= c && c <= '9') && !(c == '_')){
+                    ok = 0;
+                    break;
+                }
+            }
+            if (ok && isActive[i] && !code[i].empty()) v.push_back({businessLine[i], code[i]});
+        }
+
+        sort(v.begin(), v.end());
+
+        vector <string> codes;
+
+        for (auto &[b, s] : v) codes.push_back(s);
+
+        return codes;
+    }
+};
