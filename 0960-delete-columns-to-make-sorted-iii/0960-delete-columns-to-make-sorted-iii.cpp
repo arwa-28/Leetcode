88538ce8201fce817121fe7a419ff1005258a7f1
arwa-28 <arwam4307@gmail.com>
2025-12-22 23:13:30 +0200

    Time: 167 ms (5.1%), Space: 62.1 MB (5.1%) - LeetHub

--- 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.cpp
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
         int n = strs[0].size();
-        vector <vector <int>> dp(n, vector <int> (n, 1e9));
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int>(2, 1e9)));
+        // vector <vector <int>> dp(n, vector <int> (n, 1e9));
         function <int(int, int, bool)> go = [&](int i, int prev, bool flag){
             if (i == n) return 0;
 
-            if (dp[i][prev] != 1e9) return dp[i][prev];
+            if (dp[i][prev][flag] != 1e9) return dp[i][prev][flag];
 
-            int &ans = dp[i][prev];
+            int &ans = dp[i][prev][flag];
             if (flag){
                 bool ok = 1;
                 for (auto &s : strs) ok &= s[prev] <= s[i];
